da6bb300b1919cf8cbcd6b928dd00998152cbb62
XuanRanDev <3135419729@qq.com>
2022-11-30 19:48:42 +0800

    初步完成所有代码

--- Main.py
+++ Main.py
@@ -5,14 +5,16 @@ from hashlib import md5
 
 import requests
 
+import MessagePush
+
 pwd = os.path.dirname(os.path.abspath(__file__)) + os.sep
 
 headers = {
     "os": "android",
     "phone": "HuaWei|P30|12",
-    "appVersion": "38",
-    "Sign": "111111",
-    "cl_ip": "192.168.1.3",
+    "appVersion": "39",
+    "Sign": "Sign",
+    "cl_ip": "192.168.1.2",
     "User-Agent": "okhttp/3.14.9",
     "Content-Type": "application/json;charset=utf-8"
 }
@@ -48,47 +50,62 @@ def parseUserInfo():
 def save(user, uid, token):
     url = 'http://sxbaapp.zcj.jyt.henan.gov.cn/interface/clockindaily20220827.ashx'
 
-    d = "{\"dtype\":1,\"uid\":" + "\"" + uid + "\"," \
-        + "\"address\":" + "\"" + user["address"] + "\"" + ",\"phonetype\":" + "\"" + user["deviceType"] + "\"" \
-        + ",\"probability\":-1,\"longitude\":" + user["longitude"] + ",\"latitude\":" + user["latitude"] + "}"
+    data = {
+        "dtype": 1,
+        "uid": uid,
+        "address": user["address"],
+        "phonetype": user["deviceType"],
+        "probability": -1,
+        "longitude": user["longitude"],
+        "latitude": user["latitude"]
+    }
 
-    sign = getMd5(d + token)
+    headers["Sign"] = getMd5(json.dumps(data) + token)
 
-    headers["sign"] = sign
-
-    res = requests.post(url, headers=headers, data=json.dumps(d))
+    res = requests.post(url, headers=headers, data=json.dumps(data))
 
     if res.json()["code"] == 1001:
-        return True
-    return False
+        return True, res.json()["msg"]
+    return False, res.json()["msg"]
 
 
 def getToken():
     url = 'http://sxbaapp.zcj.jyt.henan.gov.cn/interface/token.ashx'
     res = requests.post(url, headers=headers)
-    print('请求token:', res.json()["data"]["token"])
     return res.json()["data"]["token"]
 
 
-def login(user, password, deviceId, token):
-    d = '{\"phone\":\"' + user["phone"] + "\",\"password\":" + "\"" + password + "\"," + \
-        "\"dtype\":6,\"dToken\":" + "\"" + deviceId + "\"}"
-    headers["sign"] = getMd5(d + token)
+def login(user, token):
+    password = getMd5(user["password"])
+    deviceId = user["deviceId"]
 
+    data = {
+        "phone": user["phone"],
+        "password": password,
+        "dtype": 6,
+        "dToken": deviceId
+    }
+    headers["Sign"] = getMd5((json.dumps(data) + token))
     url = 'http://sxbaapp.zcj.jyt.henan.gov.cn/interface/relog.ashx'
-    res = requests.post(url, headers=headers, data=json.dumps(d))
-    return res.json()["data"]["uid"]
+    res = requests.post(url, headers=headers, data=json.dumps(data))
+    return res.json()
 
 
 def prepareSign(user):
     if not user["enable"]:
         return
 
-    token = getToken()
-    password = getMd5(user["password"])
-    deviceId = getDeviceId()
+    headers["phone"] = user["deviceType"]
 
-    uid = login(user, password, deviceId, token)
+    token = getToken()
+
+    loginResp = login(user, token)
+
+    if loginResp["code"] != 1001:
+        print('登录账号失败，错误原因：', loginResp["msg"])
+        return
+
+    uid = loginResp["data"]["uid"]
     resp = save(user, uid, token)
 
     if resp:
@@ -98,7 +115,15 @@ def prepareSign(user):
 
 
 if __name__ == '__main__':
+
     users = parseUserInfo()
 
     for user in users:
-        prepareSign(user)
+        try:
+            prepareSign(user)
+        except Exception as e:
+            print('职校家园打卡失败，错误原因：' + str(e))
+            MessagePush.pushMessage('职校家园打卡失败',
+                                    '职校家园打卡失败,' +
+                                    '具体错误信息：' + str(e)
+                                    , user["pushKey"])
